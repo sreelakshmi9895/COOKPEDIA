22ffdbead9300b535e922b9e8b2fa9167f6448ad
sreelakshmi9895 <sreelakshmivenu9895@gmail.com>
2026-01-23 22:00:43 +0530

    ADD: Filter,search,pagination to recipe page

--- src/app/recipes/recipes.ts
+++ src/app/recipes/recipes.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { ApiService } from '../services/api-service';
+import { SearchPipe } from '../pipes/search-pipe';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-recipes',
-  imports: [Header,Footer],
+  imports: [Header,Footer,SearchPipe,FormsModule,NgxPaginationModule],
   templateUrl: './recipes.html',
   styleUrl: './recipes.css',
 })
 export class Recipes {
+
+p: number = 1
+searchKey:string=''
+allRecipes:any = signal([])
+dummyAllRecipes:any=[]
+cuisineArray:any = signal([])
+mealTypeArray:any=signal([])
+  api = inject(ApiService)
+
+  ngOnInit(){
+    this.getAllRecipes()
+  }
+
+  getAllRecipes(){
+    this.api.getAllRecipeAPI().subscribe((res:any)=>{
+      // console.log(res);
+      this.allRecipes.set(res)
+      this.dummyAllRecipes=this.allRecipes()
+      // console.log(this.allRecipes());
+      this.allRecipes().forEach((item:any)=>{
+        !this.cuisineArray().includes(item.cuisine) && this.cuisineArray().push(item.cuisine)
+      })
+      // console.log(this.cuisineArray());
+      let dummyMealTypeArray = this.allRecipes().map((item:any)=>item.mealType).flat(Infinity)
+      dummyMealTypeArray.forEach((item:any)=>{
+        !this.mealTypeArray().includes(item) && this.mealTypeArray().push(item)
+      })
+      console.log(this.mealTypeArray());
+      
+    })
+  }
+
+  filterRecipe(key:string,value:string){
+    this.allRecipes.set(this.dummyAllRecipes.filter((item:any)=>item[key]==value))
+  }
 
 }
